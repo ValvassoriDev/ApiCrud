07c8392358531e7a3707319f9d84660922c89294
Mateus Valvassori <valvassori.dev@gmail.com>
2024-02-05 16:03:56 -0300

    teste 2.0

--- obj/Debug/net8.0/ApiCrud.AssemblyInfo.cs
+++ obj/Debug/net8.0/ApiCrud.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiCrud")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39116eaefa29b645ef6f4665d59581747790a98e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiCrud")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiCrud")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
